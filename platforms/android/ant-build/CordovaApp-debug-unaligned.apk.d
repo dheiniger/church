4906b0146b189f1908b79eb1dceab72de8298e18
Daniel <daniel.r.heiniger@gmail.com>
2015-06-15 22:52:07 -0500

    delete ant-build folder contents and old image locations

--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/daniel/workspace/church/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/home/daniel/workspace/church/platforms/android/ant-build/CordovaApp.ap_ \
-/home/daniel/workspace/church/platforms/android/ant-build/classes.dex \
-/home/daniel/workspace/church/platforms/android/CordovaLib/ant-build/classes.jar \
